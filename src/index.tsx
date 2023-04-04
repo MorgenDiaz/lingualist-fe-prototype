f9f73022ef011bc381bc88f59d6ce6bccc6027a7
Morgen Diaz <morgen@webslinger.works>
2023-04-03 18:03:18 -1000

    Converted index and application files to typescript.

---
+++ src/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const ROOT_ELEMENT: Element | null = document.getElementById("root");
+
+if (ROOT_ELEMENT) {
+  const root = ReactDOM.createRoot(ROOT_ELEMENT);
+
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
