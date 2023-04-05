25191dff51b2777484bcd6712b1a6e7b018e6987
Morgen Diaz <morgen@webslinger.works>
2023-04-04 18:20:55 -1000

    Installed and configured tailwind css. Created simple UI for testing core prototype functionality.

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,29 @@ import "./App.css";
 function App(): JSX.Element {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center mb-20">
+          <p className="text-sm font-bold mb-1">Vocabulary Words</p>
+          <ul className="list-disc">
+            <li>Item 1</li>
+            <li>Item 2</li>
+            <li>Item 3</li>
+          </ul>
+        </div>
+        <div className="flex flex-col items-center">
+          <p className="text-lg font-bold mb-4">
+            Pick a word to complete the sentence:
+          </p>
+          <input
+            className="w-full max-w-lg rounded-lg border-gray-400 border-2 p-2 mb-4"
+            type="text"
+            placeholder="Vocabulary Word"
+          />
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+            Submit
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
