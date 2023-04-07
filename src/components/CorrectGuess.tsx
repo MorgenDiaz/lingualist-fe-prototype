23bc492c976d0aafd11dfbcb8330a96f684d774e
Morgen Diaz <morgen@webslinger.works>
2023-04-06 18:25:31 -1000

    Created interface for CorrectGuessComponent props.

--- src/components/CorrectGuess.tsx
+++ src/components/CorrectGuess.tsx
@@ -1,19 +1,21 @@
 import React from "react";
 
-export default function CorrectGuess({
-  word,
-  definition,
-  onPlayAgain,
-}): JSX.Element {
+interface CorrectGuessProps {
+  word: string;
+  definition: string;
+  onPlayAgain: () => void;
+}
+
+export default function CorrectGuess(CorrectGuessProps): JSX.Element {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="mb-4 text-3xl font-bold">You Guessed Correctly!</h1>
       <p className="mb-8 text-xl font-medium">
-        {word}: {definition}
+        {CorrectGuessProps.word}: {CorrectGuessProps.definition}
       </p>
       <button
         className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-        onClick={onPlayAgain}
+        onClick={CorrectGuessProps.onPlayAgain}
       >
         Play Again
       </button>
