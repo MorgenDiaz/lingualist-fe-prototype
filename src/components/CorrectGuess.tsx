50b666117330fcd6504bfd50746fd92d53cc401f
Morgen Diaz <morgen@webslinger.works>
2023-04-13 16:50:06 -1000

    When a user has selected the correct word, and is navigated to the definition page; there is an option to hear how the word is pronounced.

--- src/components/CorrectGuess.tsx
+++ src/components/CorrectGuess.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import hearIcon from "./hear-svgrepo-com.svg";
 
 interface CorrectGuessProps {
   word: string;
@@ -6,16 +7,34 @@ interface CorrectGuessProps {
   onPlayAgain: () => void;
 }
 
-export default function CorrectGuess(CorrectGuessProps): JSX.Element {
+export default function CorrectGuess(props: CorrectGuessProps): JSX.Element {
+  const synth = window.speechSynthesis;
+
+  const onSpeakWord = () => {
+    if (synth.speaking) return;
+
+    const utterance = new SpeechSynthesisUtterance(props.word);
+    utterance.rate = 0.8;
+    utterance.pitch = 1.2;
+    synth.speak(utterance);
+  };
+
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
+    <div className="flex flex-col items-center justify-center h-screen ">
       <h1 className="mb-4 text-3xl font-bold">You Guessed Correctly!</h1>
-      <p className="mb-8 text-xl font-medium">
-        {CorrectGuessProps.word}: {CorrectGuessProps.definition}
-      </p>
+      <p className="mb-8 text-xl font-medium">{props.definition}</p>
+
+      <div
+        onClick={onSpeakWord}
+        className="flex flex-col items-center p-3 mb-12 border-2 border-black rounded cursor-pointer hover:bg-gray-100"
+      >
+        <img src={hearIcon} alt="" className="mb-2 w-14" />
+        <span className="font-semibold">{props.word}</span>
+      </div>
+
       <button
         className="px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700"
-        onClick={CorrectGuessProps.onPlayAgain}
+        onClick={props.onPlayAgain}
       >
         Play Again
       </button>
